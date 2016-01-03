7877bd10a5fe41f61d62a83224b6d03daf16abdd
Jürgen Mülbert <develop@juergen-muelbert.de>
2016-01-03 18:15:19 +0100

    Little improments
    Move Package.swift on the right place. HexDump.swift extend. Settings.

--- swiftpoi/HexDump.swift
+++ swiftpoi/HexDump.swift
@@ -44,8 +44,8 @@ public class HexDump {
     // @exception IllegalArgumentException if the output stream is
     //            null
     //
-    public static func dump(data: [Int], offset: Int,
-        stream: NSOutputStream, index: Int, length: Int) {
+    public static func dump(data: NSData, offset: Int,
+        stream: String, index: Int, length: Int) {
             if (stream.isEqual(nil)) {
                 NSException.init(name: "IllegalArgumentException",
                                        reason: "connot write to nullstream",
@@ -70,10 +70,53 @@ public class HexDump {
     // @exception IllegalArgumentException if the output stream is
     //            null
     //
-    public static func dump(data: [Int], offset: Int, stream: NSOutputStream, index: Int) {
+    public static func dump(data: NSData, offset: Int, stream: String, index: Int) {
         dump(data, offset:offset, stream:stream, index:Int.max)
     }
     
+    
+    //
+    // Dumps <code>bytesToDump</code> bytes to an output stream.
+    //
+    // @param in          The stream to read from
+    // @param out         The output stream
+    // @param start       The index to use as the starting position for the left hand side label
+    // @param bytesToDump The number of bytes to output.  Use -1 to read until the end of file.
+    //
+    public static func dump(inFile: String, outFile: String, start: Int, bytesToDump: Int) {
+        var buf: NSData?
+        var pointerToUint:UnsafeMutablePointer<UInt>!
+        pointerToUint = UnsafeMutablePointer<UInt>.alloc(1)
+        if (bytesToDump == -1) {
+            do {
+                buf = try NSData(contentsOfFile: inFile)
+                buf!.writeToFile(outFile, atomically: false)
+                dump(buf!, offset:0, stream:outFile, index:start, length:buf!.length)
+            }
+            catch { /* error handling here */
+                NSException.init(name: "FileException",
+                    reason: "The file is unreadable",
+                    userInfo: nil
+                    ).raise()
+            }
+        } else {
+            do {
+                buf = try NSData(contentsOfFile: inFile)
+                var outBytes: NSData = NSData(bytesNoCopy: pointerToUint, length: bytesToDump, freeWhenDone: true)
+                outBytes.writeToFile(outFile, atomically: false)
+                dump(outBytes, offset:0, stream:outFile, index:start, length:outBytes.length)
+            }
+            catch { /* error handling here */
+                NSException.init(name: "FileException",
+                    reason: "The file is unreadable",
+                    userInfo: nil
+                    ).raise()
+            }
+            
+        }
+        
+    }
+
     //
     // dump an array of bytes to a String
     //
@@ -86,20 +129,23 @@ public class HexDump {
     //            outside the data array's bounds
     // @return output string
     //
-    public static func dump(data: [Int], offset:Int, index: Int, length: Int) ->String {
-        let space: Character = " "
-        let EOL: Character = "\n"
+    public static func dump(data: NSData, offset:Int, index: Int, length: Int) ->String {
+        let space: String = " "
+        let EOL: String = "\n"
+        var pointerToUint:UnsafeMutablePointer<UInt>!
+        pointerToUint = UnsafeMutablePointer<UInt>.alloc(1)
         
-        if (data.isEmpty || data.count == 0) {
-            return "No Data\n"
+        
+        if (data.length == 0) {
+            return "No Data" + EOL
         }
         
         let data_length:Int = (length == Int.max || length < 0 ||  index+length < 0)
-            ? data.count
-        : min(data.count, index+length)
+            ? data.length
+        : min(data.length, index+length)
         
-        if ((index < 0) || (index >= data.count)) {
-            let errorString: String = "illegal index: \(data.count) into array of length"
+        if ((index < 0) || (index >= data.length)) {
+            let errorString: String = "illegal index: \(data.length) into array of length"
             NSException.init(name: "ArrayIndexOutOfBounds",
                 reason: errorString,
                 userInfo: nil
@@ -110,13 +156,131 @@ public class HexDump {
         var display_offset: Int = offset + index
         var buffer: String
         
+     
+        
         for (var j:Int = index; j < data_length; j += 16) {
             var chars_read: Int = data_length - j
             
             if (chars_read > 16) {
                 chars_read = 16
             }
+            buffer = buffer + (NSString(format: "%8", display_offset) as String) as String
+            for (var k:Int = 0; k < 16; k++) {
+                if (k < chars_read) {
+                     // buffer = buffer + " " + (NSString(format: "%2", data.[k + j]) as String) as String
+                }
+            }
+            
+            
         }
-      return buffer
+        return "Noch nicht fertig"
+    }
+    
+    
+    // TODO ----------------------------------------------------
+    // TODO Make this
+    // TODO ----------------------------------------------------
+    public static func toAscii(dataB: UInt16) -> UInt16  {
+        let controlCharSet = NSCharacterSet.controlCharacterSet()
+        let printableCharSet = NSCharacterSet.alphanumericCharacterSet()
+        var charB: UInt16 =  (dataB & 0xFF)
+        if (controlCharSet.characterIsMember(dataB)) {
+            charB =
+        } else if (!printableCharSet.characterIsMember(dataB))  {
+            charB = (UInt16) "."
+        }
+        return dataB
+    }
+    
+    //
+    // Converts the parameter to a hex value.
+    //
+    // @param value     The value to convert
+    // @return          The result right padded with 0
+    //
+    public static func toHex(value: Int8) ->String {
+        let hexString: String =
+        (NSString(format: "%4X", value ) as String) as String
+        return hexString
+        
+    }
+
+    
+    //
+    // Converts the parameter to a hex value.
+    //
+    // @param value     The value to convert
+    // @return          The result right padded with 0
+    //
+    public static func toHex(value: Int16) ->String {
+        let hexString: String =
+        (NSString(format: "%8X", value ) as String) as String
+        return hexString
+        
+    }
+
+    
+    //
+    // Converts the parameter to a hex value.
+    //
+    // @param value     The value to convert
+    // @return          The result right padded with 0
+    // 
+    public static func toHex(value: Int32) ->String {
+        let hexString: String =
+            (NSString(format: "%16X", value ) as String) as String
+        return hexString
+        
+    }
+    
+    
+    //
+    // Converts the parameter to a hex value.
+    //
+    // @param value     The value to convert
+    // @return          The result right padded with 0
+    //
+    public static func toHex(value: Int) ->String {
+        let hexString: String =
+        (NSString(format: "%32X", value) as String) as String
+        return hexString
+        
+    }
+
+    
+  
+    //
+    // @return string of 16 (zero padded) uppercase hex chars and prefixed with '0x'
+    //
+    public static func longToHex(value: Int) -> String {
+        let hexString: String = "0x" +
+            (NSString(format: "%16X", value) as String) as String
+        return hexString
+        
+    }
+    //
+    // @return string of 8 (zero padded) uppercase hex chars and prefixed with '0x'
+    //
+    public static func intToHex(value: Int) -> String {
+        let hexString: String = "0x" +
+            (NSString(format: "%8X", (value & 0xFFFFFFFF)) as String) as String
+        return hexString
+    }
+    
+    //
+    // @return string of 4 (zero padded) uppercase hex chars and prefixed with '0x'
+    //
+    public static func shortToHex(value: Int) -> String {
+        let hexString: String = "0x" +
+            (NSString(format:"%4X", (value & 0xFFFF)) as String) as String
+        return hexString
+    }
+    
+    //
+    // @return string of 2 (zero padded) uppercase hex chars and prefixed with '0x'
+    //
+    public static func byteToHex(value: Int) -> String {
+        let hexString:String = "0x" + (NSString(format:"%2X", (value & 0xFF)) as String) as String
+        return hexString
     }
 }
